5a28f6e2feccbe90e9f49792656680ff53e3c84a
Dikhsan Dwirangga Tibong <120387198+Dikhsantibong@users.noreply.github.com>
2026-05-19 19:37:05 +0800

    perbaikan

--- public/build/assets/check-Bl9ZlKS5.js
+++
@@ -1,1 +0,0 @@
-import{I as e}from"./app-DVSa4y0A.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::redirect('/', '/login')->name('home');
 
 use App\Http\Controllers\OutagePlanController;
 use App\Http\Controllers\DailyMeetingController;
